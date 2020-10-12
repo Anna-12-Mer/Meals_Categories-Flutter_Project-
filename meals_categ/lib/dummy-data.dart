9a4f8a0155e2dee5be502653bec3aec5dcfe53f0
Anna-12-Mer <mammeramna@gmail.Com>
2020-10-12 21:28:44 +0200

    add screens

---
+++ meals_categ/lib/dummy-data.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import './models/category.dart';
+
+const DUMMY_CATEGORIES = const [
+  Category(
+    id: 'c1',
+    title: 'Italian',
+    color: Colors.amber,
+  ),
+  Category(
+    id: 'c2',
+    title: 'Quick & Easy',
+    color: Colors.amber,
+  ),
+  Category(
+    id: 'c3',
+    title: 'Hamburgers',
+    color: Colors.amber,
+  ),
+  Category(
+    id: 'c4',
+    title: 'German',
+    color: Colors.amber,
+  ),
+  Category(
+    id: 'c5',
+    title: 'Light & Lovely',
+    color: Colors.amber,
+  ),
+  Category(
+    id: 'c6',
+    title: 'Exotic',
+    color: Colors.amber,
+  ),
+  Category(
+    id: 'c7',
+    title: 'Breakfast',
+    color: Colors.amber,
+  ),
+  Category(
+    id: 'c8',
+    title: 'Asian',
+    color: Colors.amber,
+  ),
+  Category(
+    id: 'c9',
+    title: 'French',
+    color: Colors.amber,
+  ),
+  Category(
+    id: 'c10',
+    title: 'Summer',
+    color: Colors.amber,
+  ),
+];
